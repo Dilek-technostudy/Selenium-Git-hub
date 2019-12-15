32a907019f366df447c79059a13c804d4536a71c
dilek-technostudy <dileknuranyildirim@gmail.com>
2019-12-15 08:05:13 -0800

    done

--- seleniumproject/src/day1/ChromeBrowser.java
+++ seleniumproject/src/day1/ChromeBrowser.java
@@ -1,10 +1,13 @@
+package day1;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ChromeBrowser {
 
         public static void main(String[] args) {
-            System.setProperty( "webdriver.chrome.driver", "D:\\TechnoStudy\\Selenium\\ChromeDriver\\chromedriver.exe" );
+            System.setProperty( "webdriver.chrome.driver",
+                    "C:\\Users\\dilek\\IdeaProjects\\chrome\\chromedriver.exe");
             WebDriver driver = new ChromeDriver();
             driver.get( "https://www.techno.study/" );
         }
--- seleniumproject/src/day1/FireFoxBrowser.java
+++ seleniumproject/src/day1/FireFoxBrowser.java
@@ -1,10 +1,11 @@
+package day1;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
     public class FireFoxBrowser {
         public static void main(String[] args) {
-            System.setProperty( "webdriver.gecko.driver", "D:\\TechnoStudy\\Selenium\\FirefoxDriver\\geckodriver.exe" );
+            System.setProperty( "webdriver.gecko.driver", "C:\\Users\\dilek\\IdeaProjects\\Selenium\\geckodriver.exe" );
             WebDriver driver = new FirefoxDriver();
             driver.get( "https://www.techno.study/" );
         }
